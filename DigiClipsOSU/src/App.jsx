185474388dec68b539e044781d711a8b1af9ff72
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-05-17 16:40:58 -0700

    chore: Update DigiClips project partner description and add link to documentation

--- DigiClipsOSU/src/App.jsx
+++ DigiClipsOSU/src/App.jsx
@@ -33,8 +33,8 @@ function Main() {
   return (
     <main className='bg px-20 py-8 flex flex-col gap-y-3'>
       <ContentCard title='Our Project Partner' className='col-span-1'>
-          <p>DigiClips is our project partner who provides textual recording for public media such as broadcast TV and radio. They allow searching for specific text in their media sources as well as getting a transcript from scheduled media.</p>
-        </ContentCard>
+        <p>DigiClips is our project partner who provides textual recording for public media such as broadcast TV and radio. They allow searching for specific text in their media sources as well as getting a transcript from scheduled media. For example a lawyer might want to see what opposing council says in the news, DigiClips can provide that service by providing a transcript for local news channels or other relevant media sources.</p>
+      </ContentCard>
       <div className='grid grid-cols-3'>
         <ContentCard title='What Is Our Project?' className='col-span-1'>
           <p>We were tasked with helping upgrade DigiClips' administration website. Which is an internal tool that allows them to manage users, data, and company resources from a single web application.</p>
@@ -61,20 +61,12 @@ function Main() {
           <p>A JavaScript library for building user interfaces</p>
         </ContentCard>
       </div>
-      <ContentCard title='Project Use'>
-        <p>As this website is DigiClips' main administration tool to manage company resources this website is not available for public use. DigiClips does have a public website for potential customers and those wishing to learn more at&nbsp;
+      <ContentCard title='Documentation and Extra Info' className="col-span-1">
+        <p>This website isn't public but for more information about DigiClips you can check out&nbsp;
           <a href='http://www.digiclipsinc.com' target='_blank' rel='noreferrer' className='hover:text-white hover:cursor-pointer underline'>digiclipsinc.com</a>
           .
         </p>
       </ContentCard>
-      <div className='grid grid-cols-3'>
-        <ContentCard title='Documentation' className="col-span-1">
-          <p>A JavaScript library for building user interfaces</p>
-        </ContentCard>
-        <ContentCard title='Learn More' className="col-span-2">
-          <p>A JavaScript library for building user interfaces</p>
-        </ContentCard>
-      </div>
       <ContentCard title='Project Use'>
         <p>As this website is DigiClips' main administration tool to manage company resources this website is not available for public use. DigiClips does have a public website for potential customers and those wishing to learn more at&nbsp;
           <a href='http://www.digiclipsinc.com' target='_blank' rel='noreferrer' className='hover:text-white hover:cursor-pointer underline'>digiclipsinc.com</a>
@@ -98,7 +90,6 @@ function Footer() {
       <div className='mx-auto w-fit'>
         <a href='https://github.com/DigiClipsOSU/digiclipsOSU.github.io' target='_blank' rel='noreferrer' className='hover:text-white hover:cursor-pointer'>
           <div className='flex gap-3'>
-            <p className='text-xxl font-semibold'>Source Code:</p>
             <GitHubIcon className='h-12 w-auto' />
           </div>
         </a>
